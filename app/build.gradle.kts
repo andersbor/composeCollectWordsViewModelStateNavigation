d602607e341202a6e0222aa61ffb08776a270c0a
andersbor <anbo@zealand.dk>
2025-02-26 10:58:56 +0100

    gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.collectwordsviewmodelstatenavigation"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.collectwordsviewmodelstatenavigation"
